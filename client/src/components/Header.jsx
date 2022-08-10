d8ee1decdfec919e03ac1d538c8fae05e9569f0d
Adebola <bfagbowore@gmail.com>
2022-08-10 10:02:16 +0100

    completed frontend part of the app using react - create and delete client - crud for projects

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./assets/logo.png";
+
+export default function Header() {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          <div className="d-flex">
+            <img src={logo} alt="logo" className="mr-2" />
+            <div>Project Mgt</div>
+          </div>
+        </a>
+      </div>
+    </nav>
+  );
+}
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -106,6 +106,11 @@ const mutation = new GraphQLObjectType({
         id: { type: GraphQLNonNull(GraphQLString) },
       },
       resolve(parent, args) {
+        Project.find({ clientId: args.id }).then((projects) => {
+          projects.forEach((project) => {
+            project.remove();
+          });
+        });
         return Client.findByIdAndRemove(args.id);
       },
     },
